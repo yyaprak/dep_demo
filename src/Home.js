24a332b3dd9fd4599991d288249ab6be213bbcd6
ibrahim <yesilveyaprak@gmail.com>
2022-11-29 00:09:11 +0300

    changes

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,8 @@ const Home = () => {
 
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
+        { title: 'Embedded dev top tips', body: 'lorem ipsum...', author: 'ibrahim', id: 4 }
     ]);
 
     const handleDelete= (id) => {
