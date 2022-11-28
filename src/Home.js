c37cb0b62b74e85e182410cb7d07d378f8905783
ibrahim <yesilveyaprak@gmail.com>
2022-11-28 23:59:57 +0300

    changes

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,7 @@ const Home = () => {
 
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-        { title: 'Embedded dev top tips', body: 'lorem ipsum...', author: 'sergio', id: 4 }
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
     const handleDelete= (id) => {
@@ -24,7 +23,7 @@ const Home = () => {
     return (  
         
         <div className="home">
-             <BlogList blogs={blogs} title="All  Blogs !!!" handleDelete={handleDelete}/>
+             <BlogList blogs={blogs} title="All Blogs!!!" handleDelete={handleDelete}/>
              {//<BlogList blogs={blogs.filter((blog)=>blog.author==='mario')} title="Mario's  blogs!" handleDelete={handleDelete}/>
              }
         
