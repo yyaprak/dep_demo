e7d4c2a57e2fdc77691f75b735440d76191ad251
ibrahim <yesilveyaprak@gmail.com>
2022-11-28 23:51:32 +0300

    changes

--- src/Home.js
+++ src/Home.js
@@ -24,7 +24,7 @@ const Home = () => {
     return (  
         
         <div className="home">
-             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+             <BlogList blogs={blogs} title="All  Blogs !!!" handleDelete={handleDelete}/>
              {//<BlogList blogs={blogs.filter((blog)=>blog.author==='mario')} title="Mario's  blogs!" handleDelete={handleDelete}/>
              }
         
